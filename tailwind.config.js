6b781d2043439f1bddfefbb78d5487546111cc99
yasmeenTarekKhater <yasmeentarek112001@gmail.com>
2025-01-25 12:03:12 +0200

    services and about us section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,25 @@ module.exports = {
       xl: '1180px',
     },
     extend: {},
+    keyframes: {
+      move:{
+        "50%" : {transform: "translateY(-1rem)"}
+      },
+      rotate:{
+        "0%" : {transform: "rotate(0deg)"},
+        "100%" : {transform: "rotate(360deg)"}
+      },
+      scaleUp: {
+        "0%" : {transform: "scale(0.8)"},
+        "50%" : {transform: "scale(1.2)"},
+        "100%" : {transform: "scale(0.8)"}
+      }
+    },
+    animation: {
+      movingY: "move 3s ease-in-out infinite",
+      rotating: "rotate 15s linear infinite",
+      scaling: "scaleUp 3s ease-in-out infinite"
+    },
     fontFamily: {
       Jost: ['Jost', 'sans-serif'],
       EuphoriaScriptRegular: ['Euphoria Scrip', 'serif'],
